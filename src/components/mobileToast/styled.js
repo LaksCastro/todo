8d804274760e1d031094c1d5aa8074fb60859dc6
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-05 21:39:21 -0300

    Update styled.js

--- src/components/mobileToast/styled.js
+++ src/components/mobileToast/styled.js
@@ -25,15 +25,3 @@ export const ToastWrapper = styled.div`
         visibility: visible;
     }
 `
-export const Teste = styled.div`
-    position: fixed;
-    top: 87px;
-    left: 50%;
-    transform: translateX(-50%);
-    background: #505052;
-    color: #FBFBFC;
-    padding: 9px 14px;
-    border-radius: 36px;
-    font-size: 12px;
-    font-weight: normal;
-`
