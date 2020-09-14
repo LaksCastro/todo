d0df742e6da306819b2edd72ca66391329ad6192
LaksCastro <lakhsmycastro336@gmail.com>
2020-09-14 09:41:47 -0300

    Updates

---
+++ precache-manifest.e0bc5cce5616afc85fdc1030be1b29ce.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b24b45aead58e81b816d65ad8c69e3c8",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "5c239e4d6eee20a0b26e",
+    "url": "/todo/static/css/main.9a3c63ab.chunk.css"
+  },
+  {
+    "revision": "a7bb3858c1b691ef1f9a",
+    "url": "/todo/static/js/2.7002bac0.chunk.js"
+  },
+  {
+    "revision": "5c239e4d6eee20a0b26e",
+    "url": "/todo/static/js/main.24f19298.chunk.js"
+  },
+  {
+    "revision": "f2071ef809c880957f97",
+    "url": "/todo/static/js/runtime-main.95144d85.js"
+  },
+  {
+    "revision": "275682d973ee738df6e4ec071768ce23",
+    "url": "/todo/static/media/check.275682d9.svg"
+  },
+  {
+    "revision": "e4f2d6b028d93b8c7988d2132bf13ca2",
+    "url": "/todo/static/media/github.e4f2d6b0.svg"
+  },
+  {
+    "revision": "09aea0f59807f6f4f66af7f5719cba9e",
+    "url": "/todo/static/media/google.09aea0f5.svg"
+  },
+  {
+    "revision": "d8f8be8601d46e34d280ea4e44a5be1d",
+    "url": "/todo/static/media/loading.d8f8be86.svg"
+  },
+  {
+    "revision": "4223d91c869cf9e712578f3ddd864273",
+    "url": "/todo/static/media/loadingUser.4223d91c.svg"
+  },
+  {
+    "revision": "ccdf03f0b35f47d45b367e2bd6b43a68",
+    "url": "/todo/static/media/logo.ccdf03f0.svg"
+  },
+  {
+    "revision": "5bd0e45fccaf21000a09eb91fb75bf04",
+    "url": "/todo/static/media/microsoft.5bd0e45f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df24d4658950dd5e395312a17ae2f2fc.js"
+  "/todo/precache-manifest.e0bc5cce5616afc85fdc1030be1b29ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
